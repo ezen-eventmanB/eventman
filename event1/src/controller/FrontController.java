699a816e4319c9904e473d51a448ebaff0d74c3e
740 <740@DESKTOP-HUR63BL>
2021-07-08 11:28:24 +0900

    565+

--- event1/src/controller/FrontController.java
+++ event1/src/controller/FrontController.java
@@ -23,7 +23,6 @@ public class FrontController extends HttpServlet {
       String[] gubun = str.split("/");
       String str2 = gubun[1];
 
-<<<<<<< HEAD
       System.out.println("");
       System.out.println("-----Front Controller-----");
       System.out.println("uri : "+uri);
@@ -64,62 +63,18 @@ public class FrontController extends HttpServlet {
       }else if(str2.equals("EventMan_Mypage")) {
          MemberController mmc = new MemberController();
          mmc.doGet(request, response);
+         
+         //ë§ˆìŠ¤í„° íŽ˜ì´ì§€
+      }else if(str2.equals("EventMan_Master")) {
+         MasterController mc = new MasterController();
+         mc.doGet(request, response);
+         
+         //ë©”ì¸ íŽ˜ì´ì§€
+      }else if(str2.equals("EventMan_Main")) {
+         MainController mc = new MainController();
+         mc.doGet(request, response);
       }
    }
-=======
-		System.out.println("");
-		System.out.println("-----Front Controller-----");
-		System.out.println("uri : "+uri);
-		System.out.println("getContextPath() : "+request.getContextPath());
-		System.out.println("str : "+str);
-		System.out.println("str2 = "+str2);
-		System.out.println("");
-
-		request.setCharacterEncoding("UTF-8");
-		response.setContentType("text/html; charset=utf-8");
-		
-
-		if(str2.equals("EventMan_Member")) {														
-			MemberController mc = new MemberController();
-			mc.doGet(request, response);
-			
-			//°ßÀû ½ÅÃ» È­¸é ÀÌµ¿
-		}else if(str2.equals("EventMan_Cost")) {
-			CostController cc = new CostController();
-			cc.doGet(request, response);
-			
-			//¸®ºä ÆäÀÌÁö ÀÌµ¿ 
-		}else if(str2.equals("EventMan_Review")) {
-			ReviewController rc = new ReviewController();
-			rc.doGet(request, response);
-			
-			//È¸»ç¼Ò°³ ÆäÀÌÁö ÀÌµ¿
-		}else if(str2.equals("EventMan_Company")) {
-			MasterController ccc = new MasterController();
-			ccc.doGet(request, response);
-			
-			//°Ô½ÃÆÇ ÆäÀÌÁö ÀÌµ¿
-		}else if(str2.equals("EventMan_Board")) {
-			BoardController bc = new BoardController();
-			bc.doGet(request, response);
-			
-			//¸¶ÀÌÆäÀÌÁö
-		}else if(str2.equals("EventMan_Mypage")) {
-			MemberController mmc = new MemberController();
-			mmc.doGet(request, response);
-			
-			//¸¶½ºÅÍ ÆäÀÌÁö
-		}else if(str2.equals("EventMan_Master")) {
-			MasterController mc = new MasterController();
-			mc.doGet(request, response);
-			
-			//¸ÞÀÎ ÆäÀÌÁö
-		}else if(str2.equals("EventMan_Main")) {
-			MainController mc = new MainController();
-			mc.doGet(request, response);
-		}
-	}
->>>>>>> branch 'master' of https://github.com/ezen-eventmanB/eventman.git
 
 
    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
