a384d4084141b4b390fc5486c1021dac84f52dfb
759 <759@DESKTOP-5MI7NOO>
2021-07-16 10:00:44 +0900

    asdfdasfasd

--- event1/src/controller/MemberController.java
+++ event1/src/controller/MemberController.java
@@ -6,12 +6,12 @@ import java.util.ArrayList;
 import java.util.Properties;
 import java.util.Random;
 
-import javax.mail.Message;
+/*import javax.mail.Message;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMessage;*/
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -404,111 +404,81 @@ public class MemberController extends HttpServlet {
          
          
 /*	비밀번호 찾기 ACTION	*/
-      }else if(str2.equals("EventMan_Member_Find_Pw_Action.do")) {
-
-          System.out.println("if문 비밀번호 찾기");
-    	  
-    	  String id = request.getParameter("id");
-    	  String email = request.getParameter("email");
-
-    	  System.out.println("id : "+id);
-    	  System.out.println("email : "+email);
-    	  
-    	   //먼저 아이디로 회원정보를 받아오고 가져온 데이터에서 email값을 비교하여 존재하지 않으면 인증메일 보내지 못함
-    	  
-    	  EvMemberVo mvo  = new MemberServiceImpl().findpw(id,email);
-    	  
-          if(mvo==null || !mvo.getmEmail().equals(email))
-          {
-              request.setAttribute("msg", "아이디나 이메일 정보가 맞지 않습니다");
-              
-              RequestDispatcher rd =request.getRequestDispatcher("/EventMan_Member/EventMan_Member_Find_Pw.jsp ");    
-              rd.forward(request, response);
-              
-              return;
-          }else {
-          
-                  //mail server 설정
-                  String host = "smtp.naver.com";
-                  String user = "maen2@naver.com"; //자신의 네이버 계정
-                  String password = "hwang9295A!";//자신의 네이버 패스워드
-                  
-                  //메일 받을 주소
-                  String to_email = mvo.getmEmail();
-                  
-                  //SMTP 서버 정보를 설정한다.
-                  Properties props = new Properties();
-                  props.put("mail.smtp.host", host);
-                  props.put("mail.smtp.port", 465);
-                  props.put("mail.smtp.auth", "true");
-                  props.put("mail.smtp.ssl.enable", "true");
-                  
-                  //인증 번호 생성기
-                  StringBuffer temp =new StringBuffer();
-                  Random rnd = new Random();
-                  for(int i=0;i<10;i++){
-                      int rIndex = rnd.nextInt(3);
-                      switch (rIndex) {
-                      case 0:
-                          // a-z
-                          temp.append((char) ((int) (rnd.nextInt(26)) + 97));
-                          break;
-                      case 1:
-                          // A-Z
-                          temp.append((char) ((int) (rnd.nextInt(26)) + 65));
-                          break;
-                      case 2:
-                          // 0-9
-                          temp.append((rnd.nextInt(10)));
-                          break;
-                      }
-                  }
-                  String AuthenticationKey = temp.toString();
-                  System.out.println("AuthenticationKey : "+AuthenticationKey);
-                  
-                  
-                  
-                  Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
-                      protected PasswordAuthentication getPasswordAuthentication() {
-                          return new PasswordAuthentication(user,password);
-                      }
-                  });
-                  
-                  
-                  //email 전송
-	                try {
-                	  System.out.println("try입니다.");
-                	  
-              ////////////////////////////////////
-                	  
-                	  MimeMessage msg = new MimeMessage(session);
-                      System.out.println("↑exception");
-                      
-                      msg.setFrom(new InternetAddress(user, "EVENTMAN"));
-                      msg.addRecipient(Message.RecipientType.TO, new InternetAddress(to_email));
-                      
-                      //메일 제목
-                      msg.setSubject("안녕하세요 EVENTMAN 인증 메일입니다.");
-                      //메일 내용
-                      msg.setText("인증 번호는 :"+temp);
-                      
-                      Transport.send(msg);
-                      System.out.println("이메일 전송");
-                      
-                  }catch (Exception e) {
-                	  System.out.println("catch입니다.");
-                      e.printStackTrace();// TODO: handle exception
-                  }
-                  HttpSession saveKey = request.getSession();
-                  saveKey.setAttribute("AuthenticationKey", AuthenticationKey);
-                  
-                  //패스워드 바꿀때 뭘 바꿀지 조건에 들어가는 id
-                  
-                  request.setAttribute("id", id);                  
-                  RequestDispatcher rd =request.getRequestDispatcher("/EventMan_Member/EventMan_Member_Find_Pw.jsp ");    
-                  rd.forward(request, response);
-          }
-      }
+		} /*
+			 * else if(str2.equals("EventMan_Member_Find_Pw_Action.do")) {
+			 * 
+			 * System.out.println("if문 비밀번호 찾기");
+			 * 
+			 * String id = request.getParameter("id"); String email =
+			 * request.getParameter("email");
+			 * 
+			 * System.out.println("id : "+id); System.out.println("email : "+email);
+			 * 
+			 * //먼저 아이디로 회원정보를 받아오고 가져온 데이터에서 email값을 비교하여 존재하지 않으면 인증메일 보내지 못함
+			 * 
+			 * EvMemberVo mvo = new MemberServiceImpl().findpw(id,email);
+			 * 
+			 * if(mvo==null || !mvo.getmEmail().equals(email)) { request.setAttribute("msg",
+			 * "아이디나 이메일 정보가 맞지 않습니다");
+			 * 
+			 * RequestDispatcher rd
+			 * =request.getRequestDispatcher("/EventMan_Member/EventMan_Member_Find_Pw.jsp "
+			 * ); rd.forward(request, response);
+			 * 
+			 * return; }else {
+			 * 
+			 * //mail server 설정 String host = "smtp.naver.com"; String user =
+			 * "maen2@naver.com"; //자신의 네이버 계정 String password = "hwang9295A!";//자신의 네이버
+			 * 패스워드
+			 * 
+			 * //메일 받을 주소 String to_email = mvo.getmEmail();
+			 * 
+			 * //SMTP 서버 정보를 설정한다. Properties props = new Properties();
+			 * props.put("mail.smtp.host", host); props.put("mail.smtp.port", 465);
+			 * props.put("mail.smtp.auth", "true"); props.put("mail.smtp.ssl.enable",
+			 * "true");
+			 * 
+			 * //인증 번호 생성기 StringBuffer temp =new StringBuffer(); Random rnd = new Random();
+			 * for(int i=0;i<10;i++){ int rIndex = rnd.nextInt(3); switch (rIndex) { case 0:
+			 * // a-z temp.append((char) ((int) (rnd.nextInt(26)) + 97)); break; case 1: //
+			 * A-Z temp.append((char) ((int) (rnd.nextInt(26)) + 65)); break; case 2: // 0-9
+			 * temp.append((rnd.nextInt(10))); break; } } String AuthenticationKey =
+			 * temp.toString();
+			 * System.out.println("AuthenticationKey : "+AuthenticationKey);
+			 * 
+			 * 
+			 * 
+			 * Session session = Session.getDefaultInstance(props, new
+			 * javax.mail.Authenticator() { protected PasswordAuthentication
+			 * getPasswordAuthentication() { return new
+			 * PasswordAuthentication(user,password); } });
+			 * 
+			 * 
+			 * //email 전송 try { System.out.println("try입니다.");
+			 * 
+			 * ////////////////////////////////////
+			 * 
+			 * MimeMessage msg = new MimeMessage(session); System.out.println("↑exception");
+			 * 
+			 * msg.setFrom(new InternetAddress(user, "EVENTMAN"));
+			 * msg.addRecipient(Message.RecipientType.TO, new InternetAddress(to_email));
+			 * 
+			 * //메일 제목 msg.setSubject("안녕하세요 EVENTMAN 인증 메일입니다."); //메일 내용
+			 * msg.setText("인증 번호는 :"+temp);
+			 * 
+			 * Transport.send(msg); System.out.println("이메일 전송");
+			 * 
+			 * }catch (Exception e) { System.out.println("catch입니다.");
+			 * e.printStackTrace();// TODO: handle exception } HttpSession saveKey =
+			 * request.getSession(); saveKey.setAttribute("AuthenticationKey",
+			 * AuthenticationKey);
+			 * 
+			 * //패스워드 바꿀때 뭘 바꿀지 조건에 들어가는 id
+			 * 
+			 * request.setAttribute("id", id); RequestDispatcher rd
+			 * =request.getRequestDispatcher("/EventMan_Member/EventMan_Member_Find_Pw.jsp "
+			 * ); rd.forward(request, response); } }
+			 */
 
     	  
     	  
