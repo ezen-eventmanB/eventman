42d1f965462448258337dd52b90234a1381b11ac
745 <745@DESKTOP-C4J59EM>
2021-07-20 14:23:46 +0900

    황현호

--- event1/src/service/MemberServiceImpl.java
+++ event1/src/service/MemberServiceImpl.java
@@ -31,7 +31,7 @@ public class MemberServiceImpl {
       int value = 0;
 
       try {
-         String sql = "insert into EVE_MEMBER(MIDX,MID,MPWD,MNAME,MEMAIL,MPHN,MTYPE,MDATE) values(midx_seq.nextval,?,?,?,?,?,?,sysdate)";
+         String sql = "insert into EVE_MEMBER(MID,MPWD,MNAME,MEMAIL,MPHN,MTYPE,MDATE) values(?,?,?,?,?,?,now())";
          
          pstmt = conn.prepareStatement(sql);
 
